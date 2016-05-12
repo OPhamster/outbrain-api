42ef2d865a337635ef843812e6ee9a1007ba0575
Filipe Correia <filipe.lp.correia@gmail.com>
2016-05-12 16:18:35 +0100

    Fix path method for promoted links preformance report

--- lib/outbrain/api/promoted_link_performance_report.rb
+++ lib/outbrain/api/promoted_link_performance_report.rb
@@ -4,8 +4,8 @@ module Outbrain
   module Api
     class PromotedLinkPerformanceReport < Report
       PATH = 'performanceByPromotedLink'
-      
-      def self.path(campaign_id)
+
+      def self.path(options)
         "campaigns/#{options.fetch(:campaign_id)}/#{PATH}/"
       end
     end
