b9e8e4db0a4e2d9614b0683b665ae1fdb5f5ce22
Filipe Correia <filipe.lp.correia@gmail.com>
2016-05-13 11:20:35 +0100

    Some refatoring

--- lib/outbrain/api/promoted_link.rb
+++ lib/outbrain/api/promoted_link.rb
@@ -4,9 +4,15 @@ module Outbrain
   module Api
     class PromotedLink < Base
       PATH = "promotedLinks"
-
+      RESOURCE_NAME = PATH
+      
       def self.find(id)
           Request.find( PATH, id, { as: self })
+      end
+
+      def self.where(options)
+        path = "campaigns/#{options.fetch(:campaign_id)}/#{PATH}"
+        Request.where(path, options, as: self, resource_name: RESOURCE_NAME)
       end
     end
   end
